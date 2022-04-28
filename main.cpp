762657b18e4910d0f0f69738ab7dc55518f4cd93
xiangyu <a375953989@gmail.com>
2022-04-27 21:29:35 -0700

    transfer to croy

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 
 int main(int argc, char** argv) {
 
+    int num_procs, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int sizex = 5;
     int sizey = 5;
     int seed = 0;
