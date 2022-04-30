eaf92dc7506caf8e94087ee3de8030d98e6504cb
XinyiJi1 <xinyiji98@aliyun.com>
2022-04-30 16:57:59 -0700

    could run but incorrect

--- mpi_1d/mpi1d.cpp
+++ mpi_1d/mpi1d.cpp
@@ -26,6 +26,8 @@ void initiate(int rank, int sizex,int sizey, int* data, int ranks, int frequency
     local_sizex = sizex;
     total_rank = ranks;
     update_frequency = frequency;
+
+    std::cout << "[Initiate]： rank " << rank << "local_sizex: " << local_sizex << std::endl;
    
 
     //put data into board
@@ -37,6 +39,76 @@ void initiate(int rank, int sizex,int sizey, int* data, int ranks, int frequency
         board.push_back(temp_row);
     }
 
+}
+
+
+void calculate_all(int rank) {
+    int upper_size = upper_ghost.size();
+    int lower_size = lower_ghost.size();
+    int new_size = upper_size + lower_size + local_sizex;
+
+    vector<vector<int> > new_board (new_size, vector<int>(local_sizey, 0));
+
+    for (int i = 0; i < upper_size; ++i) {
+        new_board[i] = upper_ghost[i];
+    }
+    for (int i = 0; i < board.size(); ++i) {
+        new_board[i+upper_size] = board[i];
+    }
+    for (int i = 0; i < lower_size; ++i) {
+        new_board[i+upper_size+board.size()] = lower_ghost[i];
+    }
+
+    for (int t = 1 ; t <= update_frequency; ++t) {
+        int x_0 = min(t, upper_size);
+        int x_1 = new_size - t;
+        for (int i = 0; i < new_size; ++i) {
+            int row_start = i - 1 >= 0 ? i - 1 :  0;
+            int row_end = i + 1 < new_size? i + 1 : new_size - 1;
+            for (int j = 0 ; j < local_sizey; ++j) {
+                int col_start = j - 1 >= 0 ? j - 1 : 0;
+                int col_end = j + 1 < local_sizey ? j + 1 : local_sizey - 1;
+                int alive_neighbour = 0; 
+                //inlcude itself
+                for (int row = row_start; row <= row_end; row++) {
+                    for (int col = col_start; col <= col_end; col++) {
+                        alive_neighbour = alive_neighbour + new_board[row][col];
+                    }
+                }
+                alive_neighbour = alive_neighbour - new_board[i][j];
+            
+                //logistic 
+                //self is 1
+                if (new_board[i][j] == 1) {
+                    if (alive_neighbour == 2 || alive_neighbour == 3){
+                    } else {
+                        new_board[i][j] = 0;
+                    }
+                } else {
+                    if (alive_neighbour == 3) {
+                        new_board[i][j] = 1;
+                    }
+                }
+            } 
+        }
+    }
+
+    // copy back board values
+    for (int i = 0; i < board.size(); ++i) {
+
+        board[i] = new_board[i+upper_size];
+
+    }
+
+    std::cout << "after calculate all: rank " << rank << std::endl;
+
+    for (int i = 0; i < local_sizex; ++i) {
+        for (int j = 0; j < local_sizey; ++j){
+            std::cout << board[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+    
 }
 
 
@@ -139,89 +211,31 @@ void update(int rank){
     }
 
     //for confirm the accuracy of the code, get all ghost area
-    std::cout << "rank:" << rank << " upper_ghost:" <<std::endl;
-    for (int i = 0; i < upper_ghost.size(); i++) {
-        for (int j = 0; j < local_sizey; ++j){
-                std::cout << upper_ghost[i][j] << " "; 
-        }
-        std::cout << std::endl;
-    }
+    // std::cout << "rank:" << rank << " upper_ghost:" <<std::endl;
+    // for (int i = 0; i < upper_ghost.size(); i++) {
+    //     for (int j = 0; j < local_sizey; ++j){
+    //             std::cout << upper_ghost[i][j] << " "; 
+    //     }
+    //     std::cout << std::endl;
+    // }
 
-    std::cout << "rank:" << rank << " lower_ghost:" <<std::endl;
-     for (int i = 0; i < lower_ghost.size(); i++) {
-        for (int j = 0; j < local_sizey; ++j){
-                std::cout << lower_ghost[i][j] << " "; 
-        }
-        std::cout << std::endl;
-    }
+    // std::cout << "rank:" << rank << " lower_ghost:" <<std::endl;
+    //  for (int i = 0; i < lower_ghost.size(); i++) {
+    //     for (int j = 0; j < local_sizey; ++j){
+    //             std::cout << lower_ghost[i][j] << " "; 
+    //     }
+    //     std::cout << std::endl;
+    //}
     
-
-}
-
-
-void calculate_all(int rank) {
-    int upper_size = upper_ghost.size();
-    int lower_size = lower_ghost.size();
-    int new_size = upper_size + lower_size + local_sizex;
-
-    vector<vector<int> > new_board (new_size, vector<int>(local_sizey, 0));
-
-    for (int i = 0; i < upper_size; ++i) {
-        new_board[i] = upper_ghost[i];
-    }
-    for (int i = 0; i < board.size(); ++i) {
-        new_board[i+upper_size] = board[i];
-    }
-    for (int i = 0; i < lower_size; ++i) {
-        new_board[i+upper_size+board.size()] = lower_ghost[i];
-    }
-
-    for (int t = 1 ; t <= update_frequency; ++t) {
-        int x_0 = min(t, upper_size);
-        int x_1 = new_size - t;
-        for (int i = x_0; i < x_1; ++i) {
-            int row_start = i - 1 >= x_0 - 1 ? i - 1 : x_0 - 1;
-            int row_end = i + 1 < x_1 + 1? i + 1 : x_1 ;
-            for (int j = 0 ; j < local_sizey; ++j) {
-                int col_start = j - 1 >= 0 ? j - 1 : 0;
-                int col_end = j + 1 < sizey ? j + 1 : sizey - 1;
-                 int alive_neighbour = 0; 
-                //inlcude itself
-                for (int row = row_start; row <= row_end; row++) {
-                    for (int col = col_start; col <= col_end; col++) {
-                        alive_neighbour = alive_neighbour + new_board[row][col];
-                    }
-                }
-                alive_neighbour = alive_neighbour - new_board[i][j];
-            
-                //logistic 
-                //self is 1
-                if (new_board[i][j] == 1) {
-                    if (alive_neighbour == 2 || alive_neighbour == 3){
-                    } else {
-                        new_board[i][j] = 0;
-                    }
-                } else {
-                    if (alive_neighbour == 3) {
-                        new_board[i][j] = 1;
-                    }
-                }
-            } 
-        }
-    }
-
-    // copy back board values
-    for (int i = 0; i < board.size(); ++i) {
-
-        board[i] = new_board[i+upper_size];
-
-    }
-    
+    calculate_all(rank);
 }
 
 
 
-void gather(int rank, int sizex, int sizey, int* data, int* disp){
+
+
+
+void gather(int rank, int sizex, int sizey, int* data, int* disp, int* resvcounts){
     int* recv_temp;
     int* send_data = new int[local_sizex * local_sizey];
     for (int i = 0; i < local_sizex; i++) {
@@ -229,7 +243,7 @@ void gather(int rank, int sizex, int sizey, int* data, int* disp){
             send_data[i * local_sizey + j] = board[i][j];
         }
     }
-    MPI_Gatherv(send_data, local_sizex * local_sizey, MPI_INT, data, local_sizex * local_sizey, disp, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(send_data, local_sizex * local_sizey, MPI_INT, data, resvcounts, disp, MPI_INT, 0, MPI_COMM_WORLD);
     delete[] send_data;
 
 }
