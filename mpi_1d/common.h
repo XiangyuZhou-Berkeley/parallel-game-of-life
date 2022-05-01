c72f2fdd5de169dd3735034b67606d9662a09b9b
XinyiJi1 <xinyiji98@aliyun.com>
2022-04-30 17:52:07 -0700

    step 1 is correct, the other is incorrect

--- mpi_1d/common.h
+++ mpi_1d/common.h
@@ -2,5 +2,5 @@
 #include <mpi.h>
 
 void initiate(int rank, int sizex,int sizey, int* data, int ranks, int frequency = 1);
-void gather(int rank, int sizex, int sizey, int* data, int* disp);
+void gather(int rank, int sizex, int sizey, int* data, int* disp, int* resvcounts);
 void update(int rank);
